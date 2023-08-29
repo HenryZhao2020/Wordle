e675b4699148e200e8f81634a64a95c2e0d00dae
Henry <75873192+HenryZhao2020@users.noreply.github.com>
2023-08-28 19:42:16 -0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -3,25 +3,33 @@
 #include "Attr.h"
 #include "Dict.h"
 
-void restoreWindow() {
-    HWND hwnd = (HWND) File::readAll("WinID").toInt();
-    SendMessage(hwnd, WM_SYSCOMMAND, SC_RESTORE, 0);
-    SetForegroundWindow(hwnd); 
-    SetActiveWindow(hwnd); 
-    SetWindowPos(hwnd, HWND_TOP, 0, 0, 0, 0, SWP_SHOWWINDOW | SWP_NOMOVE | SWP_NOSIZE);
-    RedrawWindow(hwnd, NULL, 0, RDW_FRAME | RDW_INVALIDATE | RDW_ALLCHILDREN);
+Game *game;
+
+void raiseWidget(QWidget *widget) {
+    HWND hwnd = (HWND) widget->winId();
+    if (IsIconic(hwnd)) {
+        ShowWindow(hwnd, SW_RESTORE);
+    }
+    SetForegroundWindow(hwnd);
+}
+
+void receivedMessage(quint32 instanceId, QByteArray message) {
+    if (message == "Raise") {
+        raiseWidget(game);
+    }
 }
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    QDir::setCurrent(QApplication::applicationDirPath());
-    QDir::setCurrent("Wordle_Files");
+    SingleApplication app(argc, argv, true);
 
-    RunGuard guard("Wordle");
-    if (!guard.tryToRun()) {
-        restoreWindow();
+    if (app.isSecondary()) {
+        AllowSetForegroundWindow(DWORD(app.primaryPid()));
+        app.sendMessage("Raise");
         return 0;
     }
+
+    QDir::setCurrent(QApplication::applicationDirPath());    
+    QDir::setCurrent("Wordle_Files");
 
     QFontDatabase::addApplicationFont("../Fonts/Ubuntu_Bold.ttf");
     Dict::load();
@@ -29,13 +37,13 @@ int main(int argc, char *argv[]) {
     app.setStyle("Fusion");
     app.setStyleSheet(File::readAll("Styles.qss"));
     QObject::connect(&app, &QApplication::aboutToQuit, &app, &Attr::save);
+    QObject::connect(&app, &SingleApplication::receivedMessage, &receivedMessage);
 
-    Game game;
-    game.restore();
-    game.show();
+    game = new Game();
+    game->restore();
+    game->show();
 
     qDebug() << Attr::answer;
 
-    File::write("WinID", QString::number(game.winId()));
     return app.exec();
 }
